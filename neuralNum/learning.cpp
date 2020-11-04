693186516f3b15693cee63b1fae16bac986c2c79
Ivan <ivan.markov.2013@gmail.com>
2020-11-04 21:43:53 +0300

    0.1.1 (not working)

--- neuralNum/learning.cpp
+++ neuralNum/learning.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
-//#include <A:\\Coding\\Cpp\\Git\\neural_NUM_console\\neuralNum\\function.cpp> 
-//#include <A:\\Coding\\Cpp\\Git\\neural_NUM_console\\neuralNum\\weights.cpp> 
-
 void random_weights(const int n, double w1[], double w2[], double w3[]);
 void set_weights(const int n, double w1[], double w2[], double w3[]);
 double sigmoidFunction(double x);
+void forwardPropagation(int size_of_firstLayer, double firstLayer[], double** weights, int size_of_secondLayer, double* secondLayer);
 
 using namespace std;
 
@@ -16,7 +14,7 @@ void neural_learning()
     const int n = 5;    //Кол-во нейронов в скрытом слое
     double k = 0.3;  //Коэффициент
 
-    int input[trainSet][in] =   //Значения на входе
+    double input[trainSet][in] =   //Значения на входе
     {
         {0, 0},
         {0, 1},
@@ -31,8 +29,19 @@ void neural_learning()
         0
     };
 
-    double w_input[in][n], w_output[n]; //Весовые коэффициенты между входом и скрытым слоем и между скрытым слоем и выходом
-    random_weights(n, w_input[0], w_input[1], w_output);    //Придание им рандомных значений
+    double** w_input = new double* [in];
+    for (int count = 0; count < in; count++)
+    {
+        w_input[count] = new double [n];
+    }
+
+    double** w_output = new double* [n];
+    for (int count = 0; count < n; count++)
+    {
+        w_output[count] = new double[1];
+    }
+
+    random_weights(n, w_input[0], w_input[1], w_output[0]);    //Придание им рандомных значений
 
     int maxEpoch = 100000; //Кол-во Эпох
     int delta_load = maxEpoch / 10, loading = delta_load;   //Переменные для реализации "загрузки"
@@ -47,32 +56,23 @@ void neural_learning()
         for (int train = 0; train < trainSet; train++)
         {
             //Forward
+            double hiddenLayer[n] = {};
+            forwardPropagation(in, input[train], w_input, n, hiddenLayer);
 
-            double hiddenLayer[n], pre_result = 0;   //Расчет значений до скрытого слоя, в скрытом слое и то, что из него выйдет
-            for (int i = 0; i < n; i++)
-            {
-                double pre_hiddenLayer = 0;
-                for (int j = 0; j < in; j++)
-                {
-                    pre_hiddenLayer += input[train][j] * w_input[j][i];
-                }
-                hiddenLayer[i] = sigmoidFunction(pre_hiddenLayer);
-                pre_result += hiddenLayer[i] * w_output[i];
-            }
-            double result = sigmoidFunction(pre_result);   //Конечный результат
-            //cout << result << endl;   //Чисто для проверки
+            double result[1] = {};
+            forwardPropagation(n, hiddenLayer, w_output, 1, result);
 
             //Backward
 
-            double sigma_out = (output[train] - result) * result * (1 - result);     //Погрешность суммы до выхода, использование производной функции преобразования
+            double sigma_out = (output[train] - result[0]) * result[0] * (1 - result[0]);     //Погрешность суммы до выхода, использование производной функции преобразования
             double delta_w_output[n], sigma_in[n];
             for (int i = 0; i < n; i++)
             {
                 delta_w_output[i] = k * sigma_out * hiddenLayer[i];
-                sigma_in[i] = delta_w_output[i] * w_output[i] * hiddenLayer[i] * (1 - hiddenLayer[i]);        //Тут все еще ведутся
+                sigma_in[i] = delta_w_output[i] * w_output[i][0] * hiddenLayer[i] * (1 - hiddenLayer[i]);        //Тут все еще ведутся
                 //sigma_in[i] = (output[train] - result) * w_output[i] * hiddenLayer[i] * (1 - hiddenLayer[i]);   //дискуссии
 
-                w_output[i] += delta_w_output[i];
+                w_output[i][0] += delta_w_output[i];
             }
 
             double delta_w[in][n];
@@ -87,12 +87,29 @@ void neural_learning()
         }
     }
     cout << endl;
-    set_weights(n, w_input[0], w_input[1], w_output);
+    set_weights(n, w_input[0], w_input[1], w_output[0]);
+
+    for (int count = 0; count < in; count++)
+    {
+        delete[] w_input[count];
+    }
+    for (int count = 0; count < n; count++)
+    {
+        delete[] w_output[count];
+    }
 }
 
-void forwardPropagation(int size_of_firstLayer, double firstLayer[], double** weights, int size_of_secondLayer, double secondLayer[])
+void forwardPropagation(int size_of_firstLayer, double firstLayer[], double** weights, int size_of_secondLayer, double* secondLayer)
 {
-
+    for (int i = 0; i < size_of_secondLayer; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < size_of_firstLayer; j++)
+        {
+            sum += firstLayer[j] * weights[j][i];
+        }
+        secondLayer[i] = sigmoidFunction(sum);
+    }
 }
 
 void backwardPropagation()
